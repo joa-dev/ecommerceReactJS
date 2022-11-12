6d3c0eaf9d14f28958350fa7dbf1d09c634f358d
joaquinneis <joaquinneis@gmail.com>
2022-11-12 09:09:13 -0300

    take out the trash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import {NavBar} from './components/NavBar/NavBar'
 import {ItemListContainer} from './components/ItemListContainer/ItemListContainer'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
